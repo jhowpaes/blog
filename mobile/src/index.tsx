9189209235f39e137ed3aca18c7589cbb0ce296d
jhowpaes <jhowylli@gmail.com>
2020-04-18 01:08:33 -0300

    base project mobile

---
+++ mobile/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {AppStyle as styles} from './styles';
+
+const App = () => (
+  <View style={styles.bg}>
+    <Text>App</Text>
+  </View>
+);
+
+export default App;
---
+++ mobile/src/styles.ts
@@ -0,0 +1,9 @@
+import {ViewStyle} from 'react-native';
+
+interface AppStyles {
+  bg: ViewStyle;
+}
+
+export const AppStyle: AppStyles = {
+  bg: {},
+};
