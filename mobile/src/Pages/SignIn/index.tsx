79d1088c5d9895b7809b24a5af3e9f1e204d9901
jhowpaes <jhowylli@gmail.com>
2020-04-18 14:28:19 -0300

    added pages signIn, signUp and home

---
+++ mobile/src/Pages/SignIn/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// import { Container } from './styles';
+
+const SignIn = () => (
+  <View>
+    <Text>SignIn</Text>
+  </View>
+);
+
+export default SignIn;
---
+++ mobile/src/Pages/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// import { Container } from './styles';
+
+const SignUp = () => (
+  <View>
+    <Text>SignUp</Text>
+  </View>
+);
+
+export default SignUp;
