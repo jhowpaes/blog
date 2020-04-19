ef9fb5ac0b72caad87021f65ac2f54d6d3b08299
jhowpaes <jhowylli@gmail.com>
2020-04-18 23:38:48 -0300

    added page profile and ideas

---
+++ mobile/src/Pages/Ideas/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// import { Container } from './styles';
+
+const Ideas = () => (
+  <View>
+    <Text>Minhas ideias</Text>
+  </View>
+);
+
+export default Ideas;
---
+++ mobile/src/Pages/Profile/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+// import { Container } from './styles';
+
+const Profile = () => (
+  <View>
+    <Text>Profile</Text>
+  </View>
+);
+
+export default Profile;
