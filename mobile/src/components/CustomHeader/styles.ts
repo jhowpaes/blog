45fb02794dc817edcb65c57d220470deb9ef9736
jhowpaes <jhowylli@gmail.com>
2020-04-19 16:49:05 -0300

    added custom header

---
+++ mobile/src/components/CustomHeader/styles.ts
@@ -0,0 +1,11 @@
+import {ViewStyle} from 'react-native';
+
+interface CustomHeaderStyles {
+  holder: ViewStyle;
+}
+
+export const CustomHeaderStyle: CustomHeaderStyles = {
+  holder: {
+    alignItems: 'center',
+  },
+};
