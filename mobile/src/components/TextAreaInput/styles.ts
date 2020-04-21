317eaf053acf7d4112d6bed36201d4813444d333
jhowpaes <jhowylli@gmail.com>
2020-04-21 01:04:15 -0300

    added style textarea

---
+++ mobile/src/components/TextAreaInput/styles.ts
@@ -0,0 +1,14 @@
+import {TextStyle} from 'react-native';
+
+interface TextAreaInputStyles {
+  input: TextStyle;
+}
+
+export const TextAreaInputStyle: TextAreaInputStyles = {
+  input: {
+    height: 210,
+    backgroundColor: '#fff',
+    marginBottom: 15,
+    borderRadius: 7,
+  },
+};
