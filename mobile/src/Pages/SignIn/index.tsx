a76ddf28713e819fae7697f362ba21b8b984457a
jhowpaes <jhowylli@gmail.com>
2020-04-22 13:04:02 -0300

    implemented page signup and signin

--- mobile/src/Pages/SignIn/index.tsx
+++ mobile/src/Pages/SignIn/index.tsx
@@ -1,14 +1,25 @@
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {Image, KeyboardAvoidingView, View} from 'react-native';
+import {useDispatch} from 'react-redux';
 import LOGO from '../../assets/img/logo.png';
 import Button from '../../components/Button';
 import IText from '../../components/Input';
 import Link from '../../components/Link';
+import {signInRequest} from '../../store/modules/auth/actions';
 import {SignInStyle as styles} from './styles';
 
 const SignIn = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const passwordRef = useRef();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const formSubmit = () => {
+    dispatch(signInRequest(email, password));
+  };
 
   return (
     <KeyboardAvoidingView style={styles.bg} behavior="padding">
@@ -18,17 +29,24 @@ const SignIn = () => {
       <View style={styles.form}>
         <IText
           placeholder="Digite seu e-mail"
+          keyboardType="email-address"
           autoCorrect={false}
-          onChange={() => {}}
+          autoCapitalize="none"
+          returnKeyType="next"
+          value={email}
+          onChangeText={setEmail}
         />
         <IText
+          secureTextEntry
           placeholder="Digite sua senha"
-          autoCorrect={false}
-          type="password"
-          onChange={() => {}}
+          ref={passwordRef}
+          returnKeyType="send"
+          onSubmitEditing={formSubmit}
+          value={password}
+          onChangeText={setPassword}
         />
 
-        <Button title="Acessar" onPress={() => {}} />
+        <Button title="Acessar" onPress={formSubmit} />
 
         <Link
           title="Cadastrar conta."
--- mobile/src/Pages/SignUp/index.tsx
+++ mobile/src/Pages/SignUp/index.tsx
@@ -1,14 +1,30 @@
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {Image, KeyboardAvoidingView, View} from 'react-native';
+import {useDispatch} from 'react-redux';
 import LOGO from '../../assets/img/logo.png';
 import Button from '../../components/Button';
 import IText from '../../components/Input';
 import Link from '../../components/Link';
+import {signUpRequest} from '../../store/modules/auth/actions';
 import {SignUpStyle as styles} from './styles';
 
 const SignUp = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const usernameRef = useRef();
+
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const formSubmit = () => {
+    dispatch(signUpRequest(name, username, email, password));
+  };
 
   return (
     <KeyboardAvoidingView style={styles.bg} behavior="padding">
@@ -18,22 +34,44 @@ const SignUp = () => {
       <View style={styles.form}>
         <IText
           placeholder="Digite seu nome"
+          keyboardType="email-address"
           autoCorrect={false}
-          onChange={() => {}}
+          autoCapitalize="none"
+          returnKeyType="next"
+          value={name}
+          onChangeText={setName}
+        />
+        <IText
+          placeholder="Digite seu username"
+          ref={usernameRef}
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+          returnKeyType="next"
+          value={username}
+          onChangeText={setUsername}
         />
         <IText
           placeholder="Digite seu e-mail"
+          ref={emailRef}
+          keyboardType="email-address"
           autoCorrect={false}
-          onChange={() => {}}
+          autoCapitalize="none"
+          returnKeyType="next"
+          value={email}
+          onChangeText={setEmail}
         />
         <IText
+          secureTextEntry
           placeholder="Digite sua senha"
-          autoCorrect={false}
-          type="password"
-          onChange={() => {}}
+          ref={passwordRef}
+          returnKeyType="send"
+          onSubmitEditing={formSubmit}
+          value={password}
+          onChangeText={setPassword}
         />
 
-        <Button title="Registrar" onPress={() => {}} />
+        <Button title="Registrar" onPress={formSubmit} />
 
         <Link
           title="Já possuo conta cadastrada."
